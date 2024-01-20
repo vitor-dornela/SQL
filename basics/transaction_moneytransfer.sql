d229fb0f48d871f3ae949187e67f301bcf1622c5
vitor-dornela <vitor.dornela.m@gmail.com>
2024-01-20 18:15:12 -0300

    beginning transactions

---
+++ basics/transaction_moneytransfer.sql
@@ -0,0 +1,35 @@
+CREATE TABLE account (
+    account_id INT PRIMARY KEY,
+    account_balance DECIMAL(10, 2) NOT NULL
+);
+commit;
+
+INSERT INTO account (account_id, account_balance) VALUES
+(1, 1000),
+(2, 5000),
+(3, 600),
+(4, 900),
+(5, 1200);
+commit;
+
+-- Transfer 100 from account 2 to account 4
+start transaction;
+    update bank_account
+    set account_balance = account_balance - 100
+    where account_id = 2;
+
+    update bank_account
+    set account_balance = account_balance + 100
+    where account_id = 4;
+commit;
+
+-- This transfer will rollback
+start transaction;
+    update bank_account
+    set account_balance = account_balance - 100
+    where account_id = 2;
+
+    update bank_account
+    set account_balance = account_balance + 100
+    where account_id = 4;
+rollback transaction;
