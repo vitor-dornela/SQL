a4650e7b9f09f97a4793b089484a38e17d8063d4
vitor-dornela <vitor.dornela.m@gmail.com>
2024-01-28 23:46:10 -0300

    autoincrement

---
+++ basics/auto_increment.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employees(
+  employee_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30)
+  PRIMARY KEY(employee_id)
+);
+
+INSERT INTO employees (first_name,last_name) VALUES ('Haruna','Smith');
+INSERT INTO employees (first_name,last_name) VALUES ('Amina','Smith');
+INSERT INTO employees (first_name,last_name) VALUES ('Lewis','Adams');
+INSERT INTO employees (first_name,last_name) VALUES ('Diana','Smith');
+
+ALTER TABLE employees AUTO_INCREMENT = 50; -- change the auto increment value, default is 1
