12272108deeb2326ff920d47dfbc30876ac6e2bd
vitor-dornela <vitor.dornela.m@gmail.com>
2024-01-30 00:11:37 -0300

    Fix bug in login functionality

---
+++ basics/null.sql
@@ -0,0 +1,27 @@
+
+-- This will change Grades to 'Not Available' if it is NULL
+SELECT
+  studentID,
+  firstName,
+  lastName,
+  ISNULL(Grades, 'Not Available') AS Grades
+FROM
+  Students;
+
+
+SELECT
+  employeeID,
+  firstName,
+  NULLIF(hireDate, dateOfBirth) as hireDate,
+  NULLIF(dateOfBirth, hireDate) as dateOfBirth
+FROM
+  Employees;
+
+
+
+SELECT
+  NULLIF(hireDate, dateOfBirth) as hireDate
+FROM
+  Employees;
+
+  
