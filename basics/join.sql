429d4fdb2aeaf4c1dc403bcf406e00da2967fccb
vitor-dornela <vitor.dornela.m@gmail.com>
2024-06-12 23:46:34 -0300

    chore: Add multiple joins to join.sql

--- basics/join.sql
+++ basics/join.sql
@@ -2,3 +2,13 @@ SELECT table1.column1, table2.column2
 FROM table1
 INNER JOIN table2
 ON table1.common_column = table2.common_column;
+
+
+
+-- multiple joins
+select email, name
+from orders
+join groceries
+on orders.grocery_id = groceries.id
+join users 
+on orders.user_id  = users.id;
